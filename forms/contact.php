bc4ab0e8a42da80e5b51f2dc51356f13dab60fa8
SanjaiH <sanjaisanjai9688@gmail.com>
2024-05-14 16:25:29 +0530

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,47 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    if (isset($_POST['name']) && isset($_POST['email'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $body = $_POST['body'];
+
+        require_once "PHPMailer/PHPMailer.php";
+        require_once "PHPMailer/SMTP.php";
+        require_once "PHPMailer/Exception.php";
+
+        $mail = new PHPMailer();
+
+        //SMTP Settings
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = "dontreply899@gmail.com"; //enter you email address
+        $mail->Password = 'enburtxqcrmrusqu'; //enter you email password
+        $mail->Port = 465;
+        $mail->SMTPSecure = "ssl";
+
+        //Email Settings
+        $mail->isHTML(true);
+        $mail->setFrom($email, $name);
+        $mail->addAddress("sanjaisanjai9688@gmail.com"); //enter you email address
+        $mail->Subject = ("$email");
+        $mail->Body = ("$body" );
+
+        if ($mail->send()) {
+            $status = "success";
+            $response = "Email is sent!";
+        } else {
+            $status = "failed";
+            $response = "Something is wrong: <br><br>" . $mail->ErrorInfo;
+        }
+ 
+        echo "Your message has been sent! ";
+
+    }
+?>
+<script>
+    setTimeout(() => {
+            window.close();
+        }, 3000);
+</script>
